b0fd4dc8e349b4b11272a64519f1da56493abcb3
Andy-Miao <8716649716@qq.com>
2019-06-21 21:59:22 +0800

    q

---
+++ HMXMLYFM/Classes/Module/Home(首页)/VIP/View/HeaderAndFooter/HM_VipFooterView.swift
@@ -0,0 +1,29 @@
+//
+//  HM_VipFooterView.swift
+//  HMXMLYFM
+//
+//  Created by humiao on 2019/6/21.
+//  Copyright © 2019 humiao. All rights reserved.
+//
+
+import UIKit
+
+class HM_VipFooterView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
